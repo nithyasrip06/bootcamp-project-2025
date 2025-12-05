cc2ff4fd408d73d158bf87d6ab1e093b0946355c
Nithyasri Palanisamy <nithyasripalanisamy@gmail.com>
2025-12-04 18:30:50 -0800

    milestone 3

---
+++ bootcamp-milestone-2/src/database/blogSchema.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose"; 
+// typescript type (can also be an interface) 
+type Blog = { 
+	title: string; 
+	date: Date; 
+	description: string; 
+	image: string; 
+	imageAlt: string; 
+	slug: string; 
+	content: string; }; 
+// mongoose schema 
+const blogSchema = new Schema<Blog>({ 
+	title: { type: String, required: true }, 
+	date: { type: Date, required: false, default: new Date() }, 
+	description: { type: String, required: true }, 
+	image: { type: String, required: true }, 
+	imageAlt: { type: String, required: true }, 
+	slug: { type: String, required: true }, 
+	content: { type: String, required: true }, 
+}) 
+// defining the collection and model 
+const Blog = mongoose.models['blogs'] || mongoose.model('blogs', blogSchema); 
+
+export default Blog;
